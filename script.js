31cf8a2b41fd9ae4c66abff08d1e7bcc172db035
Tuan <maituan94@yahoo.com>
2021-11-07 21:17:16 +0700

    commit compute function and modify html, css

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+var CURRENT_YEAR = (new Date()).getFullYear();
 function compute()
 {
-    p = document.getElementById("principal").value;
+    let amount = document.getElementById("principal").value;
+
+    if (!amount || amount <= 0) {
+        window.alert("Please Enter Positive Number!!!");
+        document.getElementById("principal").focus();
+        return;
+    }
+
+    let interestRate =  document.getElementById("rate").value;
+    let amountYear = parseInt(document.getElementById("years").value);
     
+    let amountReceive = amount*interestRate*amountYear / 100;
+    document.getElementById("computation-result").innerHTML = `If you deposit <mark>${amount}</mark>, at an interest rate of <mark>${interestRate}%</mark>. <br/> You will receive an amount of <mark>${amountReceive}</mark>, in the year <mark>${CURRENT_YEAR + amountYear}</mark>`;
+}
+
+function onInterestRateChange (e) {
+    let rateValue = e.target?.value;
+    let spanText = document.getElementById("rate-result");
+    spanText.innerHTML = `${rateValue}%`;
+
 }
         
